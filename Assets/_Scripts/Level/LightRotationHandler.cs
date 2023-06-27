348749f86f2f2a0fcd82ee148d854e4a00e63ebb
Tom Jaubert <tom.jaubert@orange.fr>
2023-06-27 13:01:01 +0200

    Test Dualite jour nuit

--- Assets/_Scripts/Level/LightRotationHandler.cs
+++ Assets/_Scripts/Level/LightRotationHandler.cs
@@ -3,31 +3,28 @@ using UnityEngine;
 public class LightRotationHandler : MonoBehaviour
 {
     [SerializeField] Transform _light;
-    [SerializeField] Vector3 _axe;
     [SerializeField] TimerBoucle _timer;
 
-    private Quaternion _initialRotation;
+    private Quaternion _initilaRotation;  
     private Quaternion _targetRotation;
     private float _maxRotateTimer;
     private float _currentRotateTimer;
 
+
     private void Start()
     {
-        _initialRotation = _light.rotation;
+        _initilaRotation = _light.rotation;
+        _targetRotation = _initilaRotation * Quaternion.Euler(-5, 0, 0);
         _maxRotateTimer = _timer.MaxTimer;
-        _currentRotateTimer = _timer.CurrentTimer;
-        _targetRotation = Quaternion.AngleAxis(0f, _axe); // Rotation de -205 degrés
     }
-
     private void Update()
     {
-        _currentRotateTimer += Time.deltaTime;
-
-        if (_currentRotateTimer <= _maxRotateTimer)
+        if (_currentRotateTimer < _maxRotateTimer)
         {
-            // Interpolation linéaire entre la rotation initiale et la rotation cible
             float t = _currentRotateTimer / _maxRotateTimer;
-            _light.rotation = Quaternion.Lerp(_initialRotation, _targetRotation, t);
+            _light.rotation = Quaternion.Slerp(_initilaRotation, _targetRotation, t);
+            _currentRotateTimer += Time.deltaTime;
+            // Interpolation linéaire entre la rotation initiale et la rotation cible
         }
     }
 }

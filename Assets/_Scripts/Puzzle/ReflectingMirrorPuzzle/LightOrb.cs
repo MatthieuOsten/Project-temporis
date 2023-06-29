408068d59bc0e6fbece40102247b2d89b21880f0
Tom Jaubert <tom.jaubert@orange.fr>
2023-06-30 01:28:32 +0200

    Set des animations dans le  temple principale

--- Assets/_Scripts/Puzzle/ReflectingMirrorPuzzle/LightOrb.cs
+++ Assets/_Scripts/Puzzle/ReflectingMirrorPuzzle/LightOrb.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LightOrb : MonoBehaviour
 {
     [SerializeField] GameObject effects;
+    [SerializeField] OpenTempleHighChamber _highChamberTemple;
 
     public void ActivateOrb()
     {
         effects.SetActive(true);
+        _highChamberTemple.TempleHighChamber();
     }
 }
